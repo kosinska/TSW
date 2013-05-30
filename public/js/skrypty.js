0ea02b093273e8e1170bb70f52570b057b0de640
kosinska <osinska.km@gmail.com>
2013-05-30 21:55:09 +0200

    zarys1

---
+++ public/js/skrypty.js
@@ -0,0 +1,16 @@
+$(dosument).ready(function(){
+
+
+});
+
+function mySam()
+{
+document.getElementById("sam").innerHTML="Grasz sam^^ masz 60sek";
+this.disabled=false;document.getElementById("multi1").disabled=true;
+}
+
+function myMulti()
+{
+document.getElementById("multi").innerHTML="Grasz multi... Pierwszy wygrywa";
+this.disabled=false;document.getElementById("sam1").disabled=true;
+}
