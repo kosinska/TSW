78ed9f57c58d525cb6be47cd7146c0b357cc1808
kosinska <osinska.km@gmail.com>
2013-06-05 16:17:15 +0200

    dzialajace dwie pchelki
    single player

--- public/js/skrypty.js
+++ public/js/skrypty.js
@@ -1,19 +1,50 @@
 $(document).ready(function () {
 
-
 });
 
+/*
 var pchelka1 = '<div onmousedown="myszDol()" onmouseup='+'rPoz("right",0)'+ ' id="pchla1" class="pchla"></div>';
 var pchelka2 = '<div onmousedown="myszDol()" onmouseup='+'rPoz("left",0)' + ' id="pchla2" class="pchla"></div>';
 var pchelka3 = '<div onmousedown="myszDol()" onmouseup='+'rPion("down",0)'+ ' id="pchla3" class="pchla"></div>';
 var pchelka4 = '<div onmousedown="myszDol()" onmouseup='+'rPion("up",0)'+ ' id="pchla4" class="pchla"></div>';
-var pchelka5 = '<div onmousedown="myszDol()" onmouseup="mUp()" id="pchla5" name="pchla" class="pchla"></div>';
-var pchelka6 = '<div onmousedown="myszDol()" onmouseup="mUp()" id="pchla6" name="pchla" class="pchla"></div>';
-var pchelka7 = '<div onmousedown="myszDol()" onmouseup="mUp()" id="pchla7" name="pchla" class="pchla"></div>';
-var pchelka8 = '<div onmousedown="myszDol()" onmouseup="mUp()" id="pchla8" name="pchla" class="pchla"></div>';
+var pchelka5 = '<div onmousedown="myszDol()" id="pchla5" name="pchla" class="pchla"></div>';
+var pchelka6 = '<div onmousedown="myszDol()" id="pchla6" name="pchla" class="pchla"></div>';
+var pchelka7 = '<div onmousedown="myszDol()" id="pchla7" name="pchla" class="pchla"></div>';
+var pchelka8 = '<div onmousedown="myszDol()" id="pchla8" name="pchla" class="pchla"></div>';
 var pchelka9 = '<div id="pchla9" name="pchla" class="pchla"></div>';
+*/
 
-var liczbaElementow=5;
+var kolory_powrot=['#EE6363','#CAE1FF','yello','orange','green'];
+
+var kolory_klik=['#e93535','#B0C4DE','gray','gray','gray'];
+
+var pchly=new Array(5);
+
+
+///////////////////////////////// tablica pchel
+for(i=0; i<5;i++){
+	pchly[i]=new Array(9);
+}
+
+///////////////////////////////// tablica pchel skladowych
+for(j=0;j<2;j++){
+	for(k=0;k<9;k++){
+		if(k<2){
+	console.log('k: '+k+' j:'+ j);
+			pchly[j][k]='<div onmousedown="myszDol()" onmouseup='+'rPoz('+k+','+j+')'+ ' id="pchla'+j+k+'" class="pchla'+j+'"></div>';
+		}else if(k<4){
+			pchly[j][k]='<div onmousedown="myszDol()" onmouseup='+'rPion('+k+','+j+')'+ ' id="pchla'+j+k+'" class="pchla'+j+'"></div>';
+		}else if(k<8){
+			pchly[j][k]= '<div onmousedown="myszDol()" id="pchla'+j+k+'" class="pchla'+j+'"></div>';
+		}else {
+			pchly[j][k]= '<div id="pchla'+j+k+'" class="pchla'+j+'"></div>';
+		}	
+	}
+
+}
+
+
+var n;
 
 var dX=new Array();
 var dY=new Array();
@@ -21,32 +52,34 @@ var dY=new Array();
 var licznik_pkt=0;
 
 var licznik = new odliczanie({  
-    seconds: 5,  // number of seconds to count down
-    onUpdateStatus: function(sec){console.log(sec);}, // callback for each second
-    onCounterEnd: function(){if(licznik_pkt != 1){ alert('Czas się skończył! Przegrałeś :(');}}// final action
+    sekundy: 15,
+    onUpdateStatus: function(sec){console.log(sec);},
+    onCounterEnd: function(){if(licznik_pkt != 2){ alert('Czas się skończył! Przegrałeś :(');
+							window.location.href='/';}}
 });
 
+///////////////////////////////// odliczanie czasu
 function odliczanie(options) {
   var timer,
   instance = this,
-  seconds = options.seconds || 10,
+  sekundy = options.sekundy || 10,
   updateStatus = options.onUpdateStatus || function () {},
   counterEnd = options.onCounterEnd || function () {};
 
-  function decrementCounter() {
-    updateStatus(seconds);
-    if (seconds === 0) {
+  function zmiejszLicznik() {
+    updateStatus(sekundy);
+    if (sekundy === 0) {
       counterEnd();
       instance.stop();
     }
-    seconds--;
+    sekundy--;
   }
 
   this.start = function () {
     clearInterval(timer);
     timer = 0;
-    seconds = options.seconds;
-    timer = setInterval(decrementCounter, 1000);
+    sekundy = options.sekundy;
+    timer = setInterval(zmiejszLicznik, 1000);
   };
 
   this.stop = function () {
@@ -54,89 +87,131 @@ function odliczanie(options) {
   };
 }
 
+///////////////////////////////// klikniecie myszy
 function myszDol(){
 
-	for(i=1; i<10; i++){
-		document.getElementById("pchla"+i).style.backgroundColor="#e93535";
-		document.getElementById("pchla"+i).style.borderColor="#e93535";
-		}
+	for(i=0;i<9;i++){
+		$('#pchla'+n+i).css('backgound-color', kolory_klik[n])
+	}
 	
 }
 
-
+///////////////////////////////// odklikniecie myszy z lewej/prawej
 function rPoz(dir,n){
 
-	console.log(dX[n]);
-	console.log("Kierunek: "+dir);
-	console.trace();
-		if(dir==="right"){
+	console.log('n: '+n+' dx: '+dX + ' dir: '+ dir);
+	//console.trace();
+	
+		if(dir===0){
 			dX[n] += 50;
 				if(dX[n] >= 340) dX[n] = 340;
 				
-				if( dX[n] >= 150 && dY[n] == 150 ) { $('.pchla').remove(); licznik_pkt++;};
-				if( dX[n] >= 150 && dY[n] == 200 ) { $('.pchla').remove(); licznik_pkt++;};	
-				if(licznik_pkt == 1) alert("Wygrales!");	 
+				if( dX[n] >= 150 && dY[n] == 150 ) {
+					if(n==0)$('.pchla'+n).remove();
+					if(n==1)$('.pchla'+n).remove();
+					licznik_pkt++;};
+					
+				if( dX[n] >= 150 && dY[n] == 200 ) {
+					if(n==0)$('.pchla'+n).remove();
+					if(n==1)$('.pchla'+n).remove();
+					licznik_pkt++;};
+						
+				if(licznik_pkt == 2){
+					$('#gamefield').remove();
+					alert("Wygrales!");
+					window.location.href='/';
+				} 
 		}
-		else if (dir==="left"){
+		else if (dir===1){
 			dX[n] -= 50;
 				if(dX[n] < 0) dX[n] = 0;
 				
-				if( dX[n] <= 200 && dY[n] == 150 ) { $('.pchla').remove(); licznik_pkt++;};
-				if( dX[n] <= 200 && dY[n] == 200 ) { $('.pchla').remove(); licznik_pkt++;};
-				if(licznik_pkt == 1) alert("Wygrales!");
+				if( dX[n] <= 200 && dY[n] == 150 ) {
+					if(n==0)$('.pchla'+n).remove();
+					if(n==1)$('.pchla'+n).remove();
+					licznik_pkt++;};
+					
+				if( dX[n] <= 200 && dY[n] == 200 ) {
+					if(n==0)$('.pchla'+n).remove();
+					if(n==1)$('.pchla'+n).remove();
+					licznik_pkt++;};
+					
+				if(licznik_pkt == 2){
+					$('#gamefield').remove();
+					alert("Wygrales!");
+					window.location.href='/';
+				}
 		}
 
-	for(i=1; i<10; i++){
-		document.getElementById("pchla"+i).style.backgroundColor="#EE6363";
-		$('#pchla'+i).animate({
+	for(i=0;i<9;i++){
+		$('#pchla'+n+i).css('backgound-color', kolory_powrot[n])
+		$('#pchla'+n+i).animate({
 		left: dX[n] + 'px'
 		});
 	}
 
-	for(i=1; i<10; i++){
-		document.getElementById("pchla"+i).style.backgroundColor="#EE6363";
-		document.getElementById("pchla"+i).style.borderColor="#EE6363";
-	}
-
 }
 
 
+///////////////////////////////// odklikniecie myszy z gory/dolu
 function rPion(dir,n){
 
-	console.log(dY[n]);
+	console.log(n);
+	console.log(dY);
 	console.trace();
-		if(dir==="down"){
+		if(dir===2){
 			dY[n] += 50;
 				if(dY[n] >= 340) dY[n] = 340;
 				
-				if( dX[n] == 150 && dY[n] == 150 ) { $('.pchla').remove(); licznik_pkt++;};
-				if( dX[n] == 200 && dY[n] == 150 ) { $('.pchla').remove(); licznik_pkt++;};
-				if(licznik_pkt == 1) alert("Wygrales!");
+				if( dX[n] == 150 && dY[n] == 150 ) {
+					if(n==0)$('.pchla'+n).remove();
+					if(n==1)$('.pchla'+n).remove();
+					licznik_pkt++;};
+					
+				if( dX[n] == 200 && dY[n] == 150 ) {
+					if(n==0)$('.pchla'+n).remove();
+					if(n==1)$('.pchla'+n).remove();
+					licznik_pkt++;};
+					
+				if(licznik_pkt == 2){
+				$('#gamefield').remove();
+					alert("Wygrales!");
+					window.location.href='/';
+				}
 		}
-		else if (dir==="up"){
+		else if (dir===3){
 			dY[n] -= 50;
 				if(dY[n] < 0) dY[n] = 0;
 				
-				if( dX[n] == 150 && dY[n] == 200 ) { $('.pchla').remove(); licznik_pkt++;};
-				if( dX[n] == 200 && dY[n] == 200 ) { $('.pchla').remove(); licznik_pkt++;};
-				if(licznik_pkt == 1) alert("Wygrales!");
+				if( dX[n] == 150 && dY[n] == 200 ) {
+					if(n==0)$('.pchla'+n).remove();
+					if(n==1)$('.pchla'+n).remove();
+					licznik_pkt++;};
+					
+				if( dX[n] == 200 && dY[n] == 200 ) {
+					if(n==0)$('.pchla'+n).remove();
+					if(n==1)$('.pchla'+n).remove();
+					licznik_pkt++;};
+					
+				if(licznik_pkt == 2){
+				$('#gamefield').remove();
+					alert("Wygrales!");
+					window.location.href='/';
+				}
 		}
 
-	for(i=1; i<10; i++){
-		document.getElementById("pchla"+i).style.backgroundColor="#EE6363";
-		$('#pchla'+i).animate({
+
+	for(i=0;i<9;i++){
+		$('#pchla'+n+i).css('backgound-color', kolory_powrot[n])
+		$('#pchla'+n+i).animate({
 		top: dY[n] + 'px'
 		});
 	}
-	
-	for(i=1; i<10; i++){
-		document.getElementById("pchla"+i).style.backgroundColor="#EE6363";
-		document.getElementById("pchla"+i).style.borderColor="#EE6363";
-	}
-	
+
+
 }
 
-
+///////////////////////////////// klikniecie przycisku sam
 function sam(){
 
 	document.getElementById("sam").innerHTML="Grasz sam^^ masz 60sek";
@@ -147,27 +222,41 @@ function sam(){
 
 	$('#pole_gry').append('<div id ="gamefield"></div>');
 
-	$('#gamefield').append(pchelka5);
-	$('#gamefield').append(pchelka3);
-	$('#gamefield').append(pchelka6 + '<br / >');
+	$('#gamefield').append(pchly[0][4]);
+	$('#gamefield').append(pchly[0][2]);
+	$('#gamefield').append(pchly[0][5] + '<br / >');
 
-	$('#gamefield').append(pchelka1);
-	$('#gamefield').append(pchelka9);
-	$('#gamefield').append(pchelka2 + '<br / >');
+	$('#gamefield').append(pchly[0][0]);
+	$('#gamefield').append(pchly[0][8]);
+	$('#gamefield').append(pchly[0][1] + '<br / >');
 
-	$('#gamefield').append(pchelka7);
-	$('#gamefield').append(pchelka4);
-	$('#gamefield').append(pchelka8 + '<br / >');
-
+	$('#gamefield').append(pchly[0][6]);
+	$('#gamefield').append(pchly[0][3]);
+	$('#gamefield').append(pchly[0][7] + '<br / >');
 	pozycjaLosowa(0);
 
-	/*pozycjaLosowa(1);
-	pozycjaLosowa(2);
+	$('#gamefield').append(pchly[1][4]);
+	$('#gamefield').append(pchly[1][2]);
+	$('#gamefield').append(pchly[1][5] + '<br / >');
+
+	$('#gamefield').append(pchly[1][0]);
+	$('#gamefield').append(pchly[1][8]);
+	$('#gamefield').append(pchly[1][1] + '<br / >');
+
+	$('#gamefield').append(pchly[1][6]);
+	$('#gamefield').append(pchly[1][3]);
+	$('#gamefield').append(pchly[1][7] + '<br / >');
+	pozycjaLosowa(1);
+	
+	
+	
+	/*pozycjaLosowa(2);
 	pozycjaLosowa(3);
 	pozycjaLosowa(4);*/
 
 }
 
+///////////////////////////////// klikniecie przycisku wielu_graczy
 function wielu_graczy(){
 
 	document.getElementById("multi").innerHTML="Grasz z innymi graczami... Pierwszy wygrywa";
@@ -176,21 +265,27 @@ function wielu_graczy(){
 
 }
 
+///////////////////////////////// losowanie pozycji
 function pozycjaLosowa(n){
 
 	var rN=Math.floor(Math.random()*350);
 	var pos=Math.floor(rN/50)*50;
+	var pos1=Math.floor(rN/50)*50;
 	
-	ustawPozycje(pos,pos);
-	dX[n]=pos;
+	//wyklucz pos pudelka
+	
+	ustawPozycje(n,pos1,pos);
+	dX[n]=pos1;
 	dY[n]=pos;
 
 }
 
-function ustawPozycje(x,y){
-	for(i=1; i<10; i++){
-		$('#pchla'+i).animate({ top: y + 'px'});
-		$('#pchla'+i).animate({ left: x + 'px'});
+///////////////////////////////// ustawienie na pozycji
+function ustawPozycje(n,x,y){
+
+	for(i=0;i<9;i++){
+		$('#pchla'+n+i).animate({ top: y + 'px'});
+		$('#pchla'+n+i).animate({ left: x + 'px'});
 	}
-	
+
 }
